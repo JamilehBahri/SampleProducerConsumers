b865ab6dc1868bf029cefa04c74b299afc69cf4e
jamilehbahri <jamilebahri@yahoo.com>
2017-07-23 16:34:59 +0430

    add producer consumer problem

--- src/main/java/ir/phgint/IConsumer.java
+++
@@ -1,6 +0,0 @@
-package ir.phgint;
-
-public interface IConsumer {
-    public void getPrime();
-//    public  void  run();
-}
--- src/main/java/ir/phgint/IProducer.java
+++
@@ -1,8 +0,0 @@
-package ir.phgint;
-
-
-public interface IProducer {
-    public  void setPrime(int i);
-    public  boolean isPrimeNumber(int i);
-//    public void run();
-}
